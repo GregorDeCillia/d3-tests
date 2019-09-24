35bbd603639b556ff28bfb9a75129f7dcab811d2
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-09-24 13:40:39 +0200

    improve style
    add a bunch of semicolons, etc.

--- bar-anim-startup/graph.js
+++ bar-anim-startup/graph.js
@@ -24,32 +24,7 @@ const sample = [
     language: 'Go',
     value: 65.6,
     color: '#65cedb'
-  },
-  /*{
-    language: 'Swift',
-    value: 65.1,
-    color: '#ff6e52'
-  },
-  {
-    language: 'JavaScript',
-    value: 61.9,
-    color: '#f9de3f'
-  },
-  {
-    language: 'C#',
-    value: 60.4,
-    color: '#5d2f8e'
-  },
-  {
-    language: 'F#',
-    value: 59.6,
-    color: '#008fc9'
-  },
-  {
-    language: 'Clojure',
-    value: 59.6,
-    color: '#507dca'
-  }*/
+  }
 ];
 
 const svg = d3.select('svg#bar');
@@ -64,7 +39,7 @@ const chart = svg.append('g')
 const xScale = d3.scaleBand()
   .range([0, width])
   .domain(sample.map((s) => s.language))
-  .padding(0.4)
+  .padding(0.4);
 
 const yScale = d3.scaleLinear()
   .range([height, 0])
@@ -72,10 +47,10 @@ const yScale = d3.scaleLinear()
 
 // vertical grid lines
 const makeXLines = () => d3.axisBottom()
- .scale(xScale)
+ .scale(xScale);
 
 const makeYLines = () => d3.axisLeft()
-  .scale(yScale)
+  .scale(yScale);
 
 chart.append('g')
   .attr('transform', `translate(0, ${height})`)
@@ -91,19 +66,19 @@ chart.append('g')
   .call(makeXLines()
     .tickSize(-height, 0, 0)
     .tickFormat('')
-  )
+  );
 
 chart.append('g')
   .attr('class', 'grid')
   .call(makeYLines()
     .tickSize(-width, 0, 0)
     .tickFormat('')
-  )
+  );
 
 const barGroups = chart.selectAll()
   .data(sample)
   .enter()
-  .append('g')
+  .append('g');
 
 barGroups
   .append('rect')
@@ -114,23 +89,23 @@ barGroups
   .attr('width', xScale.bandwidth())
   .on('mouseenter', function (actual, i) {
     d3.selectAll('.value')
-      .attr('opacity', 0)
+      .attr('opacity', 0);
 
     d3.select(this)
       .transition()
       .duration(300)
       .attr('opacity', 0.6)
       .attr('x', (a) => xScale(a.language) - 5)
-      .attr('width', xScale.bandwidth() + 10)
+      .attr('width', xScale.bandwidth() + 10);
 
-    const y = yScale(actual.value)
+    const y = yScale(actual.value);
 
     line = chart.append('line')
       .attr('id', 'limit')
       .attr('x1', 0)
       .attr('y1', y)
       .attr('x2', width)
-      .attr('y2', y)
+      .attr('y2', y);
 
     barGroups.append('text')
       .attr('class', 'divergence')
@@ -139,30 +114,30 @@ barGroups
       .attr('fill', 'white')
       .attr('text-anchor', 'middle')
       .text((a, idx) => {
-        const divergence = (a.value - actual.value).toFixed(1)
+        const divergence = (a.value - actual.value).toFixed(1);
 
-        let text = ''
-          if (divergence > 0) text += '+'
-          text += `${divergence}%`
+        let text = '';
+          if (divergence > 0) text += '+';
+          text += `${divergence}%`;
 
           return idx !== i ? text : '';
-      })
+      });
 
   })
   .on('mouseleave', function () {
     d3.selectAll('.value')
-      .attr('opacity', 1)
+      .attr('opacity', 1);
 
     d3.select(this)
       .transition()
       .duration(300)
       .attr('opacity', 1)
       .attr('x', (a) => xScale(a.language))
-      .attr('width', xScale.bandwidth())
+      .attr('width', xScale.bandwidth());
 
-      chart.selectAll('#limit').remove()
-      chart.selectAll('.divergence').remove()
-  })
+      chart.selectAll('#limit').remove();
+      chart.selectAll('.divergence').remove();
+  });
 
 svg
   .append('text')
@@ -171,21 +146,21 @@ svg
   .attr('y', margin / 2.4)
   .attr('transform', 'rotate(-90)')
   .attr('text-anchor', 'middle')
-  .text('Love meter (%)')
+  .text('Love meter (%)');
 
 svg.append('text')
   .attr('class', 'label')
   .attr('x', width / 2 + margin)
   .attr('y', height + margin * 1.65)
   .attr('text-anchor', 'middle')
-  .text('Languages')
+  .text('Languages');
 
 svg.append('text')
   .attr('class', 'title')
   .attr('x', width / 2 + margin)
   .attr('y', 40)
   .attr('text-anchor', 'middle')
-  .text('Most loved programming languages in 2018')
+  .text('Most loved programming languages in 2018');
 
 svg.selectAll("rect")
   .data(sample)
@@ -194,10 +169,10 @@ svg.selectAll("rect")
   .attr("y", (g) => yScale(g.value))
   .attr('height', (g) => height - yScale(g.value))
   .delay(function(d,i){
-    console.log(d.value)
+    console.log(d.value);
     console.log(i) ;
-    return(i*100)
-  })
+    return(i*100);
+  });
 
 barGroups
   .append('text')
@@ -206,7 +181,7 @@ barGroups
   .attr('y', (a) => yScale(a.value) + 30)
   .attr('text-anchor', 'middle')
   .text((a) => `${a.value}%`)
-  .attr('opacity', 0)
+  .attr('opacity', 0);
 
 svg.selectAll(".value")
   .data(sample)
@@ -214,5 +189,5 @@ svg.selectAll(".value")
   .duration(600)
   .attr('opacity', 1)
   .delay(function(d, i){
-    return(i*100 + 800)
-  })
+    return(i*100 + 800);
+  });
--- split-bars/graph.js
+++ split-bars/graph.js
@@ -11,9 +11,9 @@ var svg = d3.select("#my_dataviz")
 .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")");
 
-const bandwith_old = 80
-const bandwith_new = 60
-const initial_color = "purple"
+const bandwith_old = 80;
+const bandwith_new = 60;
+const initial_color = "purple";
 
 svg.append("rect")
 .attrs({width:bandwith_old, height:280, x:0,
@@ -23,7 +23,7 @@ svg.append("rect")
   y:100, fill: initial_color,
   id: "right"});
 
-const x_shift = (bandwith_old-bandwith_new)/2
+const x_shift = (bandwith_old-bandwith_new)/2;
 
 function split(mode = d3.easePoly) {
   d3.select("#left")
@@ -31,14 +31,14 @@ function split(mode = d3.easePoly) {
   .ease(mode)
   .duration(1500)
   .attrs({width:bandwith_new, x:x_shift,
-    height: 180, y:200, fill:"red"})
+    height: 180, y:200, fill:"red"});
 
   d3.select("#right")
   .transition()
   .ease(mode)
   .duration(1500)
   .attrs({width:bandwith_new, x:bandwith_old+x_shift,
-    height: 380, y:00, fill: "blue"})
+    height:380, y:0, fill: "blue"});
 }
 
 function unify(mode = d3.easePoly, color=initial_color) {
@@ -47,12 +47,12 @@ function unify(mode = d3.easePoly, color=initial_color) {
   .ease(mode)
   .duration(1500)
   .attrs({width:bandwith_old, x:0,
-    height: 280, y:100, fill:color})
+    height: 280, y:100, fill:color});
 
   d3.select("#right")
   .transition()
   .ease(mode)
   .duration(1500)
   .attrs({width:bandwith_old, x:bandwith_old,
-    height: 280, y:100, fill: color})
+    height: 280, y:100, fill: color});
 }
